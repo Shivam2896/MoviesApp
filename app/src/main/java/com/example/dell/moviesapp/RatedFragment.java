fb1c7d81565ce092dd2f069634bebea5c0fb4106
DELL <shivam.aggr4@gmail.com>
2017-02-22 23:12:59 +0530

    Make Movie Thumbnail

--- app/src/main/java/com/example/dell/moviesapp/RatedFragment.java
+++ app/src/main/java/com/example/dell/moviesapp/RatedFragment.java
@@ -2,6 +2,8 @@ package com.example.dell.moviesapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +15,48 @@ import android.view.ViewGroup;
 public class RatedFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab, null);
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(
+                R.layout.recycler_view, container, false);
+
+        ContentAdapter adapter = new ContentAdapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+
+        // Set padding for Tiles
+        int cardPadding = getResources().getDimensionPixelSize(R.dimen.card_padding);
+        recyclerView.setPadding(cardPadding, cardPadding, cardPadding, cardPadding);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+
+        return recyclerView;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.item_movie, parent, false));
+        }
+    }
+
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+        // Set numbers of List in RecyclerView.
+        private static final int LENGTH = 18;
+
+        public ContentAdapter() {
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            // no-op
+        }
+
+        @Override
+        public int getItemCount() {
+            return LENGTH;
+        }
     }
 }
